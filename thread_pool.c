0557552ce1040ffdcc9ee182b36cae87b2ae2f84
Jim Gerth <jim.frederic.gerth@gmail.com>
2021-09-10 15:24:35 +0200

    thread_pool.c: Remove task queue related code
    Remove the task queue related code from the thread pool source code,
    as the task queue was moved to a separate source and header file.
    Thus also import the task queue header file.
    
    Signed-off-by: Jim Gerth <jim.frederic.gerth@gmail.com>

--- thread_pool.c
+++ thread_pool.c
@@ -2,49 +2,10 @@
 #include <stdlib.h>
 #include <pthread.h>
 
-#define TASK_QUEUE_LENGTH 5
+#include "task_queue.h"
+
+
 #define THREAD_POOL_SIZE 5
-
-typedef void (*task_action_t)(void);
-
-typedef struct {
-    task_action_t action;
-} task_t;
-
-typedef struct {
-    task_t tasks[TASK_QUEUE_LENGTH];
-    int head;
-    int tail;
-} task_queue_t;
-
-task_queue_t task_queue;
-
-void
-task_queue_init(void) {
-    task_queue.head = 0;
-    task_queue.tail = 0;
-}
-
-void
-task_queue_enqueue(task_t task) {
-    if ((task_queue.head + 1) % TASK_QUEUE_LENGTH == task_queue.tail) {
-        fprintf(stderr, "Could not enqueue task: The task queue is full!\n");
-    }
-
-    task_queue.tasks[task_queue.head++] = task;
-    task_queue.head %= TASK_QUEUE_LENGTH;
-}
-
-task_t
-task_queue_dequeue(void) {
-    if (task_queue.head == task_queue.tail) {
-        fprintf(stderr, "Could not dequeue task: The task queue is empty!\n");
-    }
-
-    task_t task = task_queue.tasks[task_queue.tail++];
-    task_queue.tail %= TASK_QUEUE_LENGTH;
-    return task;
-}
 
 pthread_t thread_pool[THREAD_POOL_SIZE];
 

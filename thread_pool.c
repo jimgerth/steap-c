066a340ce7b71cb8c45d1e8b524c169ff0d5b8d0
Jim Gerth <jim.frederic.gerth@gmail.com>
2021-09-10 13:05:26 +0200

    thread_pool.c: Implement task queue
    Signed-off-by: Jim Gerth <jim.frederic.gerth@gmail.com>

--- thread_pool.c
+++ thread_pool.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define TASK_QUEUE_LENGTH 100
+#define TASK_QUEUE_LENGTH 5
 
 typedef void (*task_action_t)(void);
 
@@ -9,7 +9,36 @@ typedef struct {
     task_action_t action;
 } task_t;
 
-task_t task_queue[TASK_QUEUE_LENGTH];
+typedef struct {
+    task_t tasks[TASK_QUEUE_LENGTH];
+    int head;
+    int tail;
+} task_queue_t;
+
+task_queue_t task_queue;
+
+void
+task_queue_init(void) {
+    task_queue.head = 0;
+    task_queue.tail = 0;
+}
+
+int
+task_queue_enqueue(task_t task) {
+    if ((task_queue.head + 1) % TASK_QUEUE_LENGTH == task_queue.tail) {
+        return 1;
+    }
+
+    task_queue.tasks[task_queue.head++] = task;
+    return 0;
+}
+
+task_t
+task_queue_dequeue(void) {
+    if (task_queue.head != task_queue.tail) {
+        return task_queue.tasks[task_queue.tail++];
+    }
+}
 
 int
 main(void) {

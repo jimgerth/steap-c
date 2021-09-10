8a497cab067151f0be1d2da83164239eafa6a377
Jim Gerth <jim.frederic.gerth@gmail.com>
2021-09-10 15:20:44 +0200

    task_queue.h: Extract task queue into header
    Extract all of the typedefs, defines and functions regarding the task queue
    into a separate header, as the thread_pool.c source file got cluttered and
    the task queue is a quite well defined separate entity from the thread pool.
    
    Signed-off-by: Jim Gerth <jim.frederic.gerth@gmail.com>

---
+++ task_queue.h
@@ -0,0 +1,30 @@
+#ifndef TASK_QUEUE_H
+#define TASK_QUEUE_H
+
+
+#define TASK_QUEUE_LENGTH 5
+
+typedef void (*task_action_t)(void);
+
+typedef struct {
+    task_action_t action;
+} task_t;
+
+typedef struct {
+    task_t tasks[TASK_QUEUE_LENGTH];
+    int head;
+    int tail;
+} task_queue_t;
+
+task_queue_t task_queue;
+
+void
+task_queue_init(void);
+
+void
+task_queue_enqueue(task_t task);
+
+task_t
+task_queue_dequeue(void);
+
+#endif

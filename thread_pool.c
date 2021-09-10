ae3ab1787ef5afa201073bc8f0a74a56aca1422c
Jim Gerth <jim.frederic.gerth@gmail.com>
2021-09-10 12:30:31 +0200

    thread_pool.c: Define task type and task queue
    Signed-off-by: Jim Gerth <jim.frederic.gerth@gmail.com>

--- thread_pool.c
+++ thread_pool.c
@@ -1,8 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define TASK_QUEUE_LENGTH 100
+
+typedef void (*task_action_t)(void);
+
+typedef struct {
+    task_action_t action;
+} task_t;
+
+task_t task_queue[TASK_QUEUE_LENGTH];
+
 int
 main(void) {
-    printf("Hello, world!\n");
     return EXIT_SUCCESS;
 }

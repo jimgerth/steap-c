3e216effecc407d4b4b8295748c92b89a7b5226a
Jim Gerth <jim.frederic.gerth@gmail.com>
2021-09-11 11:57:38 +0200

    task_queue: Make en- and dequeueing thread safe
    Introduce a mutex for the task queue, making the enqueue and dequeue
    operations thread safe, by locking and unlocking the mutex before and
    after the critical sections respectfully.
    Also introduce a condition variable signalling to dequeue() that the
    queue is not empty, making dequeue a blocking call until it successfully
    retrieves a task.
    
    Signed-off-by: Jim Gerth <jim.frederic.gerth@gmail.com>

--- task_queue.c
+++ task_queue.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <pthread.h>
 
 #include "task_queue.h"
 
@@ -7,10 +8,14 @@ void
 task_queue_init(void) {
     task_queue.head = 0;
     task_queue.tail = 0;
+    pthread_mutex_init(&task_queue.mutex, NULL);
+    pthread_cond_init(&task_queue.not_empty, NULL);
 }
 
 void
 task_queue_enqueue(task_t task) {
+    pthread_mutex_lock(&task_queue.mutex);
+
     if ((task_queue.head + 1) % TASK_QUEUE_LENGTH == task_queue.tail) {
         fprintf(stderr, "Could not enqueue task: The task queue is full!\n");
         return;
@@ -18,17 +23,25 @@ task_queue_enqueue(task_t task) {
 
     task_queue.tasks[task_queue.head++] = task;
     task_queue.head %= TASK_QUEUE_LENGTH;
+
+    pthread_cond_signal(&task_queue.not_empty);
+
+    pthread_mutex_unlock(&task_queue.mutex);
 }
 
 task_queue_dequeue_status
 task_queue_dequeue(task_t *task) {
-    if (task_queue.head == task_queue.tail) {
-        fprintf(stderr, "Could not dequeue task: The task queue is empty!\n");
-        return DEQUEUE_FAILURE;
+    pthread_mutex_lock(&task_queue.mutex);
+
+    while (task_queue.head == task_queue.tail) {
+        pthread_cond_wait(&task_queue.not_empty, &task_queue.mutex);
     }
 
     task_t dequeued = task_queue.tasks[task_queue.tail++];
     task_queue.tail %= TASK_QUEUE_LENGTH;
+
+    pthread_mutex_unlock(&task_queue.mutex);
+
     *task = dequeued;
     return DEQUEUE_SUCCESS;
 }
--- task_queue.h
+++ task_queue.h
@@ -1,6 +1,8 @@
 #ifndef TASK_QUEUE_H
 #define TASK_QUEUE_H
 
+#include <pthread.h>
+
 
 #define TASK_QUEUE_LENGTH 5
 
@@ -14,6 +16,8 @@ typedef struct {
     task_t tasks[TASK_QUEUE_LENGTH];
     int head;
     int tail;
+    pthread_mutex_t mutex;
+    pthread_cond_t not_empty;
 } task_queue_t;
 
 task_queue_t task_queue;

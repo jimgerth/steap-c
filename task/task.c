531ba7d1900a7b5dcd106df61826f64b614f4ea6
Jim Gerth <jim.frederic.gerth@gmail.com>
2021-09-26 22:52:47 +0200

    task/task.c: Implement task_create() and -destroy()
    Implement the newly introduced task_create() and task_destroy() functions.
    Also add NULL pointer value checks to the task_init() and task_execute() to make
    them more resistant to invalid handles.
    
    Signed-off-by: Jim Gerth <jim.frederic.gerth@gmail.com>

--- task/task.c
+++ task/task.c
@@ -1,13 +1,44 @@
+#include <stdlib.h>
+
 #include "task.h"
 
 
+task_t *
+task_create(task_action_t action, void *argument) {
+    task_t *task;
+    if ((task = malloc(sizeof (task_t))) == NULL) {
+        return NULL;
+    }
+
+    task_init(task, action, argument);
+    return task;
+}
+
+void
+task_destroy(task_t **task) {
+    if (task == NULL) {
+        return;
+    }
+
+    free(*task);
+    *task = NULL;
+}
+
 void
 task_init(task_t *task, task_action_t action, void *argument) {
+    if (task == NULL) {
+        return;
+    }
+
     task->action = action;
     task->argument = argument;
 }
 
 void
 task_execute(task_t *task) {
+    if (task == NULL) {
+        return;
+    }
+
     task->action(task->argument);
 }
